4e2f402d08454a4a967bd2bf91f9bd52c424f4ae
catsnipe <catsnipe@gmail.com>
2021-06-21 23:52:59 +0900

    release

---
+++ Assets/Plugins/uindies/Debug/SimpleStats/ScriptCounter.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+/// <summary>
+/// Measure the processing time of the loop script that Unity executes every frame.
+/// 毎フレーム実行するスクリプトの処理時間計測
+/// http://tsubakit1.hateblo.jp/entry/2018/04/17/233000
+/// </summary>
+public class ScriptCounter
+{
+    // event definition
+    public struct UpdateTimeStart { }
+    public struct UpdateTimeEnd { }
+
+    public class LoopsInfo
+    {
+        public string	LoopName;
+        public int		FrameCount;
+        public float	PastTime;
+        public float	TotalTime;
+        public float	AverageTime;
+    }
+    
+    static List<LoopsInfo> loops;
+    static float time;
+    
+    /// <summary>
+    /// Insert a unique event at the beginning and end of the loop event.
+    /// ループイベントの最初と最後に独自イベントを挟む
+    /// </summary>
+    [RuntimeInitializeOnLoadMethod()]
+    static void Initialize()
+    {
+        var playerLoop = UnityEngine.LowLevel.PlayerLoop.GetDefaultPlayerLoop();
+        
+        loops = new List<LoopsInfo>();
+        foreach (var loop in playerLoop.subSystemList)
+        {
+            LoopsInfo group = new LoopsInfo();
+            group.LoopName  = loop.type.Name;
+
+            loops.Add(group);
+        }
+        
+        // 計測開始
+        UnityEngine.LowLevel.PlayerLoopSystem presys = new UnityEngine.LowLevel.PlayerLoopSystem()
+        {
+            type = typeof(UpdateTimeStart),
+            updateDelegate = () => { StartMeasure(); }
+        };
+        // 計測終了
+        UnityEngine.LowLevel.PlayerLoopSystem[] postsys = new UnityEngine.LowLevel.PlayerLoopSystem[]
+        {
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(0); }
+            },
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(1); }
+            },
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(2); }
+            },
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(3); }
+            },
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(4); }
+            },
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(5); }
+            },
+            new UnityEngine.LowLevel.PlayerLoopSystem()
+            {
+                type = typeof(UpdateTimeEnd),
+                updateDelegate = () => { EndMeasure(6); }
+            },
+        };
+        
+        if (playerLoop.subSystemList.Length != postsys.Length)
+        {
+            Debug.LogError($"unity in current version is unsupported.");
+            return;
+        }
+
+        // In Unity 2019.4
+        //
+        // initialization
+        // earlyupdate
+        // fixedupdate
+        // preupdate
+        // update
+        // prelateupdate
+        // postlateupdate
+        for (int group = 0; group < playerLoop.subSystemList.Length; group++)
+        {
+            var updateSystem = playerLoop.subSystemList[group];
+            var subSystem    = new List<UnityEngine.LowLevel.PlayerLoopSystem>(updateSystem.subSystemList);
+
+            subSystem.Insert(0, presys);
+            subSystem.Add(postsys[group]);
+    
+            updateSystem.subSystemList      = subSystem.ToArray();
+            playerLoop.subSystemList[group] = updateSystem;
+        }
+        
+        UnityEngine.LowLevel.PlayerLoop.SetPlayerLoop(playerLoop);
+    }
+    
+    /// <summary>
+    /// Get information for each group of PlayerLoops.
+    /// PlayerLoops のグループごとの情報を取得
+    /// </summary>
+    public static List<LoopsInfo> GetPlayerLoopsInfo()
+    {
+        return loops;
+    }
+    
+    /// <summary>
+    /// Start measurement.
+    /// 計測開始
+    /// </summary>
+    static void StartMeasure()
+    {
+        time = Time.realtimeSinceStartup;
+    }
+    
+    /// <summary>
+    /// End measurement.
+    /// 計測終了
+    /// </summary>
+    /// <param name="loopsNo">PlayerLoops group number</param>
+    static void EndMeasure(int loopsNo)
+    {
+        LoopsInfo group = loops[loopsNo];
+        
+        group.FrameCount++;
+        group.PastTime     = Time.realtimeSinceStartup - time;
+        group.TotalTime   += group.PastTime;
+        group.AverageTime  = group.TotalTime / group.FrameCount;
+    }
+    
+}
---
+++ Assets/Plugins/uindies/Debug/SimpleStats/SimpleStats.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+/// <summary>
+/// Simple statistics for unity debug
+/// デバッグ用パフォーマンスモニタ
+/// </summary>
+public class SimpleStats : MonoBehaviour
+{
+    public enum eType
+    {
+        Default,
+        Script,
+    }
+
+    [SerializeField]
+    TextMeshProUGUI ScriptHeader     = null;
+    [SerializeField]
+    TextMeshProUGUI FPSValue         = null;
+    [SerializeField]
+    TextMeshProUGUI MemoryValue      = null;
+    [SerializeField]
+    TextMeshProUGUI RenderValue      = null;
+    [SerializeField]
+    TextMeshProUGUI ScriptValue      = null;
+    [SerializeField]
+    GameObject      TypeDefault      = null;
+    [SerializeField]
+    GameObject      TypeScript       = null;
+    [SerializeField]
+    float           EveryDrawingTime = 0.2f;
+    [SerializeField]
+    eType           Type             = eType.Default;
+
+    StringBuilder sb;
+    float fps;
+    float renderTime;
+    float renderCallsCount;
+    
+    /// <summary>
+    /// start
+    /// </summary>
+    void Start()
+    {
+        sb = new StringBuilder();
+        
+        if (ScriptHeader != null)
+        {
+            string text = "";
+            foreach (var loop in ScriptCounter.GetPlayerLoopsInfo())
+            {
+                text += $"{loop.LoopName}\r\n";
+            }
+            ScriptHeader.SetText(text);
+        }
+
+        FPSValue?.SetText($"---");
+        MemoryValue?.SetText($"---");
+        RenderValue?.SetText($"---");
+        ScriptValue?.SetText($"---");
+
+        if (Debug.isDebugBuild == false)
+        {
+            return;
+        }
+        
+        StartCoroutine(fpsChecker());
+        StartCoroutine(renderChecker());
+
+        StartCoroutine(draw());
+    }
+
+    /// <summary>
+    /// on validate
+    /// </summary>
+    void OnValidate()
+    {
+#if UNITY_EDITOR
+        if (Type == eType.Default)
+        {
+            TypeDefault.SetActive(true);
+            TypeScript.SetActive(false);
+        }
+        else
+        {
+            TypeDefault.SetActive(false);
+            TypeScript.SetActive(true);
+        }
+#endif
+    }
+    
+    /// <summary>
+    /// fps checker
+    /// </summary>
+    IEnumerator fpsChecker()
+    {
+        float frameCount = 0;
+        float prevTime   = 0.0f;
+        
+        fps = 0.0f;
+
+        while (true)
+        {
+            frameCount++;
+            float time = Time.realtimeSinceStartup - prevTime;
+
+            // n秒ごとに計測
+            if (time >= EveryDrawingTime)
+            {
+                fps = frameCount / time;
+            
+                frameCount = 0;
+                prevTime = Time.realtimeSinceStartup;
+            }
+
+            yield return null;
+        
+        }
+    }
+
+    /// <summary>
+    /// render checker
+    /// </summary>
+    IEnumerator renderChecker()
+    {
+        Recorder recoder = Recorder.Get("Camera.Render");
+
+        while (true)
+        {
+            renderTime       = recoder.elapsedNanoseconds * 0.000001f;
+            renderCallsCount = recoder.sampleBlockCount;
+
+            yield return new WaitForSeconds(EveryDrawingTime);
+        }
+    }
+
+    /// <summary>
+    /// draw
+    /// </summary>
+    IEnumerator draw()
+    {
+        while (true)
+        {
+            if (Type == eType.Default)
+            {
+                // fps
+                FPSValue?.SetText($"{fps}");
+
+                // memory
+                if (MemoryValue != null)
+                {
+                    string monoheap  = (Profiler.GetMonoHeapSizeLong() / 1024).ToString("#,0");
+                    string monoused  = (Profiler.GetMonoUsedSizeLong() / 1024).ToString("#,0");
+                    string allocated = (Profiler.GetTotalAllocatedMemoryLong() / 1024).ToString("#,0");
+                    string reserved  = (Profiler.GetTotalReservedMemoryLong() / 1024).ToString("#,0");
+                    string graphics  = (Profiler.GetAllocatedMemoryForGraphicsDriver() / 1024).ToString("#,0");
+                    string crlf      = System.Environment.NewLine;
+                    MemoryValue?.SetText($"{monoheap} / {monoused} Kb{crlf}{allocated} / {reserved} Kb{crlf}{graphics} Kb");
+                }
+        
+                // render
+                RenderValue?.SetText($"{renderTime:F3}ms ({renderCallsCount} Calls)");
+            }
+            else
+            if (Type == eType.Script)
+            {
+                // script
+                if (ScriptValue != null)
+                {
+                    sb.Clear();
+            
+                    foreach (var loop in ScriptCounter.GetPlayerLoopsInfo())
+                    {
+                        float avg = loop.AverageTime * 1000;
+                        sb.AppendLine($"{avg:F4}ms");
+                    }
+                    ScriptValue?.SetText(sb.ToString());
+                }
+            }
+
+            yield return new WaitForSeconds(EveryDrawingTime);
+        }
+    }
+}
